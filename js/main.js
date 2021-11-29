4e0b13a60ab8971d0c31f186d4f667f93f3ef510
butanokeiton <72650078+penguinspizza@users.noreply.github.com>
2021-11-29 15:04:38 +0900

    Minimize main.js
    不要な行を削除しました。

--- js/main.js
+++ js/main.js
@@ -2,23 +2,3 @@
 $(function () {
   $("#videoPlay").YTPlayer(); //動画の呼び出し部分に指定したID
 });
-
-// IDにリンクしたときの位置をナビバーの高さ分だけ下にずらす
-$(function () {
-  $('a[href^="#"]').click(function () {
-    var adjust = -80;
-    var speed = 0;
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top + adjust;
-    $("body,html").animate({ scrollTop: position }, speed, "swing");
-    return false;
-  });
-});
-
-// ナビバー押したら閉じる
-$(".navbar-nav>li>a , .dropdown-menu>a").on("click", function () {
-  if (this.id != "navbarDropdown") {
-    $(".navbar-collapse").collapse("hide");
-  }
-});
